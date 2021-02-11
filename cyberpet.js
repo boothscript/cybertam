db8db08cd7c16a1546be67ee0730a3577d709434
Stephen Booth <stephenboothdev@gmail.com>
2021-02-11 13:45:26 +0000

    add js

--- cyberpet.js
+++ cyberpet.js
@@ -5,16 +5,29 @@ function isFailure(chance) {
   return randomValue() < chance;
 }
 
+const petIsDead = new CustomEvent("petIsDead", {
+  detail: {
+    text: "Oh no, your pet is dead. RIP.",
+  },
+});
+const petIsHungry = new CustomEvent("petAlert", {
+  detail: {
+    text: "Your pet looks a little hungry!",
+  },
+});
+const propsDidUpdate = new Event("propsDidUpdate");
+
 class CyberPet {
-  constructor(name) {
+  constructor(name, domElementRef) {
     this.name = name;
     this.health = randomValue();
     this.food = randomValue();
     this.water = randomValue();
     this.happiness = randomValue();
     this.energy = randomValue();
+    this.domElementRef = domElementRef;
   }
-  eat() {
+  feed() {
     this.food = Math.min(this.food + 20, 100);
     this.health = Math.min(this.health + 10);
     this.useProperty("happiness", 10);
@@ -43,16 +56,29 @@ class CyberPet {
     this.useProperty("energy", 5);
   }
   tick() {
-    this.useProperty("happiness", 10);
-    this.useProperty("food", 10);
-    this.useProperty("water", 10);
-    this.useProperty("energy", 5);
+    const tickInterval = setInterval(() => {
+      console.log("ticking");
+      this.useProperty("happiness", 10);
+      this.useProperty("food", 10);
+      this.useProperty("water", 10);
+      this.useProperty("energy", 5);
+      this.domElementRef.dispatchEvent(propsDidUpdate);
+      if (this.food <= 0) {
+        this.domElementRef.dispatchEvent(petIsHungry);
+      }
+
+      if (this.health <= 0) {
+        this.domElementRef.dispatchEvent(petIsDead);
+        clearInterval(tickInterval);
+      }
+    }, 3000);
   }
 
   useProperty(property, value) {
     if (value > this[property]) {
       this[property] = 0;
-      this.health -= value - this[property];
+      this.health = Math.max(this.health - (value - this[property]), 0);
+      console.log({ health: this.health });
     } else {
       this[property] -= value;
     }
---
+++ script.js
@@ -0,0 +1,66 @@
+const petContainer = document.querySelector("#pet-container");
+const textBar = document.querySelector("#text-bar");
+const healthStatusBar = document.querySelector("#health");
+const energyStatusBar = document.querySelector("#energy");
+const foodStatusBar = document.querySelector("#food");
+const waterStatusBar = document.querySelector("#water");
+const happinessStatusBar = document.querySelector("#happiness");
+const playButton = document.querySelector("#play");
+const feedButton = document.querySelector("#feed");
+const waterButton = document.querySelector("#drink");
+const napButton = document.querySelector("#nap");
+const petButton = document.querySelector("#pet");
+
+// create cyber pet
+const cyberPet = new CyberPet("Tam", petContainer);
+
+// BUTTON EVENT HANDLERS
+const actionButtons = [
+  playButton,
+  feedButton,
+  waterButton,
+  napButton,
+  petButton,
+];
+
+// CyberPet EVENT HANDLERS
+
+petContainer.addEventListener("propsDidUpdate", () => {
+  console.log("testing");
+  updateStatusBars();
+  // possible animation
+});
+petContainer.addEventListener("petAlert", (e) => {
+  textBar.innerHTML = e.detail.text;
+});
+
+petContainer.addEventListener("petIsDead", (e) => {
+  textBar.innerHTML = e.detail.text;
+});
+
+actionButtons.forEach((actionButton) => {
+  actionButton.addEventListener("click", (e) => {
+    cyberPet[actionButton.id]();
+    updateStatusBars();
+  });
+});
+
+const statusBars = [
+  healthStatusBar,
+  energyStatusBar,
+  foodStatusBar,
+  waterStatusBar,
+  happinessStatusBar,
+];
+
+function updateStatusBars() {
+  statusBars.forEach((statusBar) => {
+    statusBar.style.width = `${cyberPet[statusBar.id]}%`;
+  });
+}
+
+// display initial values
+updateStatusBars();
+console.log("here");
+// start pet
+cyberPet.tick();
